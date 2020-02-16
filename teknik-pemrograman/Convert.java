7479db40edfdd06ae6a825ecd2b7708fda8410a4
Agus Stiawan <agus.stiawan2211@gmail.com>
2020-02-16 20:25:06 +0700

    change flow Current.java program

--- teknik-pemrograman/Convert.java
+++ teknik-pemrograman/Convert.java
@@ -2,6 +2,18 @@ import java.util.Scanner;
 import java.text.NumberFormat;
 import java.util.Locale;
 
+class Currency {
+    int id;
+    String name;
+    double value;
+
+    Currency(int _id, String _name, double _value) {
+        id = _id;
+        name = _name;
+        value = _value;
+    }
+}
+
 class Convert {
     private static Scanner input = new Scanner(System.in);
 
@@ -10,48 +22,78 @@ class Convert {
         System.out.flush();
     }
 
-    public static void main(final String[] args) {
+    public static void main(String[] args) {
         Convert.clearScreen();
 
-        int money;
-        final double riyal = 3651.53;
-        System.out.println("Konversi Rupiah ke Riyal");
+        Currency currencies[] = new Currency[3];
+        currencies[0] = new Currency(1, "Dollar", 13693.75);
+        currencies[1] = new Currency(2, "Riyal", 3651.53);
+        currencies[2] = new Currency(3, "Euro", 14831.50);
+
+        int rupiah;
+        Currency currencySelected;
+
+        System.out.println("Konversi Mata Uang");
+
+        outerLoop: {
+            while (true) {
+                System.out.println("------------------");
+                for (Currency currency : currencies) {
+                    System.out.format("[%d] %s\n", currency.id, currency.name);
+                }
+                System.out.println("------------------");
+                System.out.println("Pilih tujuan konversi [1/2]:");
+
+                String currencyIdString = input.nextLine();
+
+                try {
+                    int currencyId = Integer.parseInt(currencyIdString);
+
+                    if (currencyId > currencies.length || currencyId <= 0) {
+                        Convert.clearScreen();
+                        System.out.println("Format angka salah!");
+                    } else {
+                        for (Currency currency : currencies) {
+                            if (currency.id == currencyId) {
+                                currencySelected = currency;
+                                break outerLoop;
+                            }
+                        }
+                    }
+                } catch (NumberFormatException e) {
+                    Convert.clearScreen();
+                    System.out.println("Format angka tidak valid!");
+                }
+            }
+        }
+
+        Convert.clearScreen();
 
         while (true) {
             System.out.println("Masukkan jumlah nominal uang dalam rupiah :");
 
-            String moneyString = input.nextLine();
+            String rupiahString = input.nextLine();
 
             try {
-                money = Integer.parseInt(moneyString);
+                rupiah = Integer.parseInt(rupiahString);
 
-                if (money <= 0) {
+                if (rupiah <= 0) {
                     Convert.clearScreen();
                     System.out.println("Angka yang diinputkan minimal 0");
                 } else {
                     break;
                 }
             } catch (NumberFormatException e) {
-                System.out.println("\nFormat angka salah!");
-                System.out.println("Coba lagi [y/n] :");
-                String retry = input.nextLine();
-
-                System.out.print(retry);
-
-                if (!retry.equals("y")) {
-                    input.close();
-                    return;
-                }
-
                 Convert.clearScreen();
+                System.out.println("\nFormat angka salah!");
             }
         }
 
         NumberFormat nf = NumberFormat.getInstance(new Locale("id", "ID"));
-        String prettyRupiah = nf.format(money);
-        String prettyRiyal = nf.format((int) (money / riyal));
+        String prettyRupiah = nf.format(rupiah);
+        String prettyRiyal = nf.format(rupiah / currencySelected.value);
 
-        System.out.format("\nRp %s dalam Riyal adalah‎ %s Riyal\n", prettyRupiah, prettyRiyal);
+        System.out.format("\nRp %s dalam %s adalah‎ %s\n", prettyRupiah, currencySelected.name, prettyRiyal);
 
         input.close();
     }
