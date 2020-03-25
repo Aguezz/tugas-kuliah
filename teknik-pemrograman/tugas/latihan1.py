b463bbf8c1b16e20ca2a541075bdaeddc3381294
Agus Stiawan <agus.stiawan2211@gmail.com>
2020-03-25 14:09:10 +0700

    update

--- teknik-pemrograman/tugas/latihan1.py
+++ teknik-pemrograman/tugas/latihan1.py
@@ -1,23 +1,12 @@
-# '''
-# @author Agus Stiawan - H96219038
-# '''
-# n = int(input("Input number : "))
-# print("The first n natural number are :", n)
+'''
+@author Agus Stiawan - H96219038
+'''
+n = int(input("Input number : "))
+print("The first n natural number are :", n)
 
-# sum = 0
-# for i in range(1, n+1):
-#     print(i)
-#     sum += i
-
-# print("The sum of Natural Number upto", n, "terms :", sum)
-
-n = input("Input number : ")
-n = int(n)
-print("The first n natural number are : " + str(n))
-
-total = 0
+sum = 0
 for i in range(1, n+1):
     print(i)
-    total += i
+    sum += i
 
-print("The sum of Natural Number upto " + str(n) + " terms :" + str(total))
+print("The sum of Natural Number upto", n, "terms :", sum)
